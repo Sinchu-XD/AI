a66f081f021b063e9c4f5d0b9a1295b63dfc3b47
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-03-05 12:13:18 +0530

    Create Bot.py

---
+++ Abhi/Bot.py
@@ -0,0 +1,21 @@
+import os, asyncio
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from Abhi.Handlers import start, help_command, handle_message, newchat_command, init_chatgpt
+from Abhi.Filters import AuthorizedUserFilter
+
+def start_bot():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init_chatgpt())
+    app = Application.builder().token(os.getenv("BOT_TOKEN")).build()
+    
+    # Commands
+    app.add_handler(CommandHandler("start", start, filters=AuthorizedUserFilter()))
+    app.add_handler(CommandHandler("help", help_command, filters=AuthorizedUserFilter()))
+    app.add_handler(CommandHandler("new", newchat_command, filters=AuthorizedUserFilter()))
+    
+    # Messages
+    app.add_handler(MessageHandler( AuthorizedUserFilter() & ~filters.COMMAND & filters.TEXT, handle_message))
+    
+    # Run the bot until the user presses Ctrl-C
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
